3ca2340a85efd0a486993523b5bca440a6de7589
kocejko <35913400+kocejko@users.noreply.github.com>
2019-01-02 10:45:35 +0100

    Add files via upload

--- DataConverter.py
+++ DataConverter.py
@@ -13,27 +13,36 @@ import re
 #from tkinter import *
 import tkinter
 from tkinter import filedialog
+from scipy import signal
 
 def open_convert():
     root = tkinter.Tk()
     root.withdraw()
     file_name = filedialog.askopenfilename( filetypes = (("all","*.*"),("Text","*.txt")))
-    #i = 0
-    s_no = 1500
+    i = 0
+    s_no = 1500 # spodziewana liczba probek, dlugosc sygnału
     results = []
-    out = np.zeros([0,1500,4])
+    out = np.zeros([0,s_no,6])
     with open(file_name) as csvfile:
         for line in csvfile:
             if len(line.split()) == 0:  
                 print("pusto")
             elif '#' in line:
+                i+=1
+                #print(i)
                 if len(results) > 1:
                     #print(''.join(results))
-                    print(len(results))
-                    if len(results) == s_no:
-                        a = np.array(results[0:], dtype = float)
-                        a2 = a.reshape(1,1500,4)
-                        out = np.vstack([out,a2])
+                    #print(len(results))
+                    #if len(results) == s_no:
+                    #    #a_res = signal.resample(results, s_no)
+                    #    a = np.array(results[0:], dtype = float)
+                    #    a2 = a.reshape(1,s_no,6)
+                    #    out = np.vstack([out,a2])
+                    #if len(results) != s_no:
+                    a_res = signal.resample(results, s_no)
+                    a = np.array(a_res[0:], dtype = float)
+                    a2 = a.reshape(1,s_no,6)
+                    out = np.vstack([out,a2])
                     results = []
             else:
                 temp_l = line.strip()                
@@ -42,7 +51,7 @@ def open_convert():
         #print(len(results))
         if len(results) == s_no:
             a = np.array(results[0:], dtype = float)
-            a2 = a.reshape(1,1500,4)
+            a2 = a.reshape(1,s_no,6)
             out = np.vstack([out,a2])
         
         #print(a2)
